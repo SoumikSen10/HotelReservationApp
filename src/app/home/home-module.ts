aa1103990cdd5b9d85a6bbe38d6ab115a46fd31c
Soumik Sen <mastersoumik2003@gmail.com>
2026-04-13 20:34:23 +0530

    Pushing entire codebase

---
+++ src/app/home/home-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Home } from './home';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule,RouterModule,Home],
+})
+export class HomeModule {}
